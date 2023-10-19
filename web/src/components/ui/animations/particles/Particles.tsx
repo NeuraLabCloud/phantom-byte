c20f6ffa50073cbdb8319cea77198102ba717a78
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-10-19 00:05:25 -0400

    Update Particles.tsx

--- web/src/components/ui/animations/particles/Particles.tsx
+++ web/src/components/ui/animations/particles/Particles.tsx
@@ -4,26 +4,34 @@ import gsap from 'gsap';
 interface ParticlesProps {}
 
 const Particles: FC<ParticlesProps> = () => {
+	// Create a reference to the canvas element
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 
 	useEffect(() => {
+		// Get the canvas and its context
 		const canvas = canvasRef.current;
 		if (!canvas) return;
 
 		const ctx = canvas.getContext('2d');
 		if (!ctx) return;
 
+		// Set the width and height of the canvas to match the window dimensions
 		const cw = (canvas.width = window.innerWidth);
 		const ch = (canvas.height = window.innerHeight);
-		const dots = Array.from({ length: 450 }, () => ({
+
+		// Create an array of dots with initial positions and sizes
+		const dots = Array.from({ length: 500 }, () => ({
 			x: cw * Math.random(),
 			y: -10,
 			r: gsap.utils.random(1.5, 4.5, 0.1),
 		}));
-		const dur = 100;
-		const hue = 275;
-		const mPos = { x: cw / 2, y: ch };
 
+		// Define other necessary variables for the animation
+		const dur = 100; // Duration of animation
+		const hue = 275; // Base hue value for the dots
+		const mPos = { x: cw / 2, y: ch }; // Mouse position for interactive effect
+
+		// Function to draw a single dot
 		const drawDot = (x: number, y: number, r: number) => {
 			const dist = Math.abs(x - mPos.x) + Math.abs(y - mPos.y);
 			ctx.fillStyle = `hsl(${hue}, 100%, ${
@@ -34,11 +42,13 @@ const Particles: FC<ParticlesProps> = () => {
 			ctx.fill();
 		};
 
+		// Function to clear the canvas and redraw all dots
 		const redraw = () => {
 			ctx.clearRect(0, 0, cw, ch);
 			dots.forEach((dot) => drawDot(dot.x, dot.y, dot.r));
 		};
 
+		// GSAP animation timeline for dot movement and properties
 		const timeline = gsap.timeline({ onUpdate: redraw }).from(dots, {
 			duration: dur,
 			ease: 'none',
@@ -49,11 +59,14 @@ const Particles: FC<ParticlesProps> = () => {
 			stagger: { from: 'random', amount: dur, repeat: -1 },
 		});
 
+		// Event listener for mouse movement to update the mouse position
 		canvas.onpointermove = (e) => gsap.to(mPos, { x: e.offsetX, y: e.offsetY });
 
+		// Fast-forward the animation to the end to avoid initial flickering
 		timeline.seek(dur);
 	}, []);
 
+	// Render the canvas with fixed position to cover the whole viewport
 	return (
 		<canvas
 			ref={canvasRef}
