ee4d7d088db869c8e9d8aec94971306b08cb8a0e
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-10-21 16:22:52 -0400

    formatting

--- web/supabase/migrations/20231021153918_add_new_user_trigger.sql
+++ web/supabase/migrations/20231021153918_add_new_user_trigger.sql
@@ -1,21 +1,21 @@
 -- inserts a row into public.profiles
-CREATE FUNCTION public.handle_new_user()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-SECURITY DEFINER
-SET search_path = public
-AS $$
--- This will be passed to the supabase function body
-BEGIN
-  -- We want to pass the id, email to the new clients object
-  INSERT INTO public.clients (user_id, email)
-  VALUES (new.id, new.email);
-  RETURN NEW;
-END;
-$$;
+CREATE FUNCTION public.handle_new_user() RETURNS TRIGGER LANGUAGE plpgsql SECURITY DEFINER
+SET
+  search_path = public AS $ $ -- This will be passed to the supabase function body
+  BEGIN -- We want to pass the id, email to the new clients object
+INSERT INTO
+  public.clients (user_id, email)
+VALUES
+  (new.id, new.email);
 
+RETURN NEW;
+
+END;
+
+$ $;
 
 -- trigger the function every time a user is created
 create trigger on_auth_user_created
-  after insert on auth.users
-  for each row execute procedure public.handle_new_user();
+after
+insert
+  on auth.users for each row execute procedure public.handle_new_user();
--- web/supabase/migrations/20231021200243_add_policys.sql
+++ web/supabase/migrations/20231021200243_add_policys.sql
@@ -1,36 +1,99 @@
 -- Allow users to access their own data in all tables
-CREATE POLICY select_policy_clients ON public.clients 
-  FOR SELECT
+CREATE POLICY select_policy_clients ON public.clients FOR
+SELECT
+  USING (user_id = auth.uid());
+
+CREATE POLICY select_policy_projects ON public.projects FOR
+SELECT
   USING (
-    user_id = auth.uid()
+    id IN (
+      SELECT
+        project_1
+      FROM
+        clients
+      WHERE
+        user_id = auth.uid()
+      UNION
+      SELECT
+        project_2
+      FROM
+        clients
+      WHERE
+        user_id = auth.uid()
+      UNION
+      SELECT
+        project_3
+      FROM
+        clients
+      WHERE
+        user_id = auth.uid()
+    )
   );
 
-CREATE POLICY select_policy_projects ON public.projects 
-  FOR SELECT
+CREATE POLICY select_policy_logs ON public.logs FOR
+SELECT
   USING (
-    id IN (SELECT project_1 FROM clients WHERE user_id = auth.uid() 
-           UNION 
-           SELECT project_2 FROM clients WHERE user_id = auth.uid() 
-           UNION 
-           SELECT project_3 FROM clients WHERE user_id = auth.uid())
+    project_id IN (
+      SELECT
+        id
+      FROM
+        projects
+      WHERE
+        id IN (
+          SELECT
+            project_1
+          FROM
+            clients
+          WHERE
+            user_id = auth.uid()
+          UNION
+          SELECT
+            project_2
+          FROM
+            clients
+          WHERE
+            user_id = auth.uid()
+          UNION
+          SELECT
+            project_3
+          FROM
+            clients
+          WHERE
+            user_id = auth.uid()
+        )
+    )
   );
 
-CREATE POLICY select_policy_logs ON public.logs 
-  FOR SELECT
+CREATE POLICY select_policy_tokens ON public.tokens FOR
+SELECT
   USING (
-    project_id IN (SELECT id FROM projects WHERE id IN (SELECT project_1 FROM clients WHERE user_id = auth.uid() 
-                   UNION 
-                   SELECT project_2 FROM clients WHERE user_id = auth.uid() 
-                   UNION 
-                   SELECT project_3 FROM clients WHERE user_id = auth.uid()))
-  );
-
-CREATE POLICY select_policy_tokens ON public.tokens 
-  FOR SELECT
-  USING (
-    project_id IN (SELECT id FROM projects WHERE id IN (SELECT project_1 FROM clients WHERE user_id = auth.uid() 
-                   UNION 
-                   SELECT project_2 FROM clients WHERE user_id = auth.uid() 
-                   UNION 
-                   SELECT project_3 FROM clients WHERE user_id = auth.uid()))
+    project_id IN (
+      SELECT
+        id
+      FROM
+        projects
+      WHERE
+        id IN (
+          SELECT
+            project_1
+          FROM
+            clients
+          WHERE
+            user_id = auth.uid()
+          UNION
+          SELECT
+            project_2
+          FROM
+            clients
+          WHERE
+            user_id = auth.uid()
+          UNION
+          SELECT
+            project_3
+          FROM
+            clients
+          WHERE
+            user_id = auth.uid()
+        )
+    )
   );
