514f8868ec9e2968bf114fd2fadf1c6c393ce52e
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-10-24 19:13:42 -0400

    fixes

--- web/lib/supabase/client.ts
+++ web/lib/supabase/client.ts
@@ -1,18 +1,13 @@
+import { createBrowserClient } from "@supabase/ssr";
 import { Database } from "./database-types";
 import { useCallback } from "react";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const createClient = () =>
   useCallback(
     () =>
-      createClientComponentClient<Database>(
-        {
-          supabaseKey,
-          supabaseUrl
-        }
+      createBrowserClient<Database>(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       ),
     [],
   )();
