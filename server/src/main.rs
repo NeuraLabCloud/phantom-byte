33575acd8ab37374ea0f9ead63b78173f9a007be
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-10-26 02:03:23 -0400

    server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,19 @@
-fn main()
-{
-    println!("Hello, world!");
+use actix_web::{get, web::ServiceConfig};
+use shuttle_actix_web::ShuttleActixWeb;
+use tracing::info;
+
+#[get("/")]
+async fn hello_world() -> &'static str {
+    "PhantomByte REST API v0.1.0"
+}
+
+#[shuttle_runtime::main]
+async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    let config = move |cfg: &mut ServiceConfig| {
+        cfg.service(hello_world);
+    };
+
+    info!("Starting PhantomByte REST API v0.1.0");
+
+    Ok(config.into())
 }
